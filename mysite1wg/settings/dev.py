3ff31bec5a3fa7e47ce5da76ef52233ab02e6989
wayfrr <wayfrr@gmail.com>
2021-09-08 23:52:59 +0530

    first commit

---
+++ mysite1wg/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-lq*p92f1a=fvwwq5sbgmufoy2b6^o11a9b!3x_h^1um^#ozj6p'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
